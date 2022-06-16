cd122145e205793c5dd885653b75d26fbc12c3b4
Rafael Gaviolli <bygamerrcg@hotmail.com>
2022-06-15 22:55:55 -0300

    calc commit

---
+++ TarefaCalculoMedia/src/br/com/rgaviolli/media.java
@@ -0,0 +1,19 @@
+package br.com.rgaviolli;
+
+public class media {
+    public static void main(String args[]){
+
+
+
+        int N1 = 5;
+        int N2 = 7;
+        int N3 = 9;
+        int N4 = 3;
+
+        int N5 = (N1+N2+N3+N4) / 4;
+
+        System.out.println("A sua media final : " + N5);
+
+    }
+
+}
